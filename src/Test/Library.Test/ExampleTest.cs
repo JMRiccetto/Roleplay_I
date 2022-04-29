50a8e2f258cdf566fdd46041e3a683d23398b8ea
Juan-Tabarez <jtabarez6@gmail.com>
2022-04-29 13:33:31 -0300

    se agregan mas test

--- src/Test/Library.Test/ExampleTest.cs
+++ src/Test/Library.Test/ExampleTest.cs
@@ -9,7 +9,10 @@ namespace Test.Library
     {
 
 
-        //Tests del personaje Warrior 
+        //Tests Warrior 
+
+
+        // Name tests
 
         [Test]
         public void validName()
@@ -38,8 +41,10 @@ namespace Test.Library
             Assert.AreEqual(expected, actual);
         }
 
+        // Health tests
+
         [Test]
-        public void healthTest()
+        public void healthTestWithoutItems()
         {
             Warrior warrior = new Warrior("Ragnar");
             int expected = 100;
@@ -47,15 +52,217 @@ namespace Test.Library
             Assert.AreEqual(expected, actual);
         }
 
-         [Test]
-        public void swordTest()
+        [Test]
+        public void healthTestWithItems()
         {
             Warrior warrior = new Warrior("Ragnar");
+            Sword sword = new Sword(20,0);
+            Breastplate breastplate = new Breastplate(0,20);
+            warrior.ChangeSword(sword);
+            warrior.ChangeBreastplate(breastplate);
             int expected = 100;
             int actual = warrior.Health;
             Assert.AreEqual(expected, actual);
         }
 
+        // Item tests
+
+        [Test]
+        public void isNullSword()
+        {
+            Warrior warrior = new Warrior("Ragnar");
+            Assert.IsNull(warrior.Sword);
+        }
+
+        [Test]
+        public void changeSwordTest()
+        {
+            Warrior warrior = new Warrior("Ragnar");
+            Sword sword = new Sword(20,0);
+            warrior.ChangeSword(sword);
+            Assert.IsNotNull(warrior.Sword);
+        }
+
+        [Test]
+        public void removeSwordTest()
+        {
+            Warrior warrior = new Warrior("Ragnar");
+            Sword sword = new Sword(20,0);
+            warrior.ChangeSword(sword);
+            warrior.RemoveSword();
+            Assert.IsNull(warrior.Sword);
+        }
+
+        [Test]
+        public void isNullBreastplate()
+        {
+            Warrior warrior = new Warrior("Ragnar");
+            Assert.IsNull(warrior.Breastplate);
+        }
+
+        [Test]
+        public void changeBreastplateTest()
+        {
+            Warrior warrior = new Warrior("Ragnar");
+            Breastplate breastplate = new Breastplate(0,20);
+            warrior.ChangeBreastplate(breastplate);
+            Assert.IsNotNull(warrior.Sword);
+        }
+
+        [Test]
+        public void removeBreastplateTest()
+        {
+            Warrior warrior = new Warrior("Ragnar");
+            Breastplate breastplate = new Breastplate(0,20);
+            warrior.ChangeBreastplate(breastplate);
+            warrior.RemoveBreastplate();
+            Assert.IsNull(warrior.Sword);
+        }
+
+        // GetAttack() method tests
+
+        [Test]
+        public void attackValueWithoutItems()
+        {
+            Warrior warrior = new Warrior("Ragnar");
+            int expected = 15;
+            int actual = warrior.GetAttack();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void attackValueWithItems()
+        {
+            Warrior warrior = new Warrior("Ragnar");
+            Sword sword = new Sword(20,0);
+            Breastplate breastplate = new Breastplate(0,20);
+            warrior.ChangeSword(sword);
+            warrior.ChangeBreastplate(breastplate);
+            int expected = 35;
+            int actual = warrior.GetAttack();
+            Assert.AreEqual(expected, actual);
+        }
+
+        // attackWarrior method tests 
+
+        [Test]
+        
+        // Without items
+        public void attackWarrior1()    
+        {
+            Warrior warrior1 = new Warrior("Ragnar");
+            Warrior warrior2 = new Warrior("Bjorn");
+            warrior1.attackWarrior(warrior2);
+            int expected = 85;
+            int actual = warrior2.Health;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+
+        // Attacker with sword  , Attacked without breastplate
+        public void attackWarrior2()    
+        {
+            Warrior warrior1 = new Warrior("Ragnar");
+            Warrior warrior2 = new Warrior("Bjorn");
+            Sword sword = new Sword(20,0);
+            warrior1.ChangeSword(sword);
+            warrior1.attackWarrior(warrior2);
+            int expected = 65;
+            int actual = warrior2.Health;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+
+        // Attacker without sword  , Attacked with breastplate
+        public void attackWarrior3()    
+        {
+            Warrior warrior1 = new Warrior("Ragnar");
+            Warrior warrior2 = new Warrior("Bjorn");
+            Breastplate breastplate = new Breastplate(0,20);
+            warrior2.ChangeBreastplate(breastplate);
+            warrior1.attackWarrior(warrior2);
+            int expected = 100;
+            int actual = warrior2.Health;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+
+        // Attacker with sword  , Attacked with breastplate
+        public void attackWarrior4()    
+        {
+            Warrior warrior1 = new Warrior("Ragnar");
+            Warrior warrior2 = new Warrior("Bjorn");
+            Sword sword = new Sword(25,0);
+            Breastplate breastplate = new Breastplate(0,20);
+            warrior1.ChangeSword(sword);
+            warrior2.ChangeBreastplate(breastplate);
+            warrior1.attackWarrior(warrior2);
+            int expected = 95;
+            int actual = warrior2.Health;
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        // attackWizard method tests 
+
+
+        [Test]
+        
+        public void attackWizard1()    
+        {
+            Warrior warrior = new Warrior("Ragnar");
+            Wizard wizard = new Wizard("Merlín");
+            warrior.attackWizard(wizard);
+            int expected = 55;
+            int actual = wizard.Health;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+
+        public void attackWizard2()    
+        {
+            Warrior warrior = new Warrior("Ragnar");
+            Wizard wizard = new Wizard("Merlín");
+            Sword sword = new Sword(20,0);
+            warrior.ChangeSword(sword);
+            warrior.attackWizard(wizard);
+            int expected = 35;
+            int actual = wizard.Health;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+
+        public void attackWizard3()    
+        {
+            Warrior warrior = new Warrior("Ragnar");
+            Wizard wizard = new Wizard("Merlín");
+            Cape cape = new Cape(0,15);
+            wizard.ChangeCape(cape);
+            warrior.attackWizard(wizard);
+            int expected = 70;
+            int actual = wizard.Health;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+
+        public void attackWizard4()    
+        {
+            Warrior warrior = new Warrior("Ragnar");
+            Wizard wizard = new Wizard("Merlín");
+            Sword sword = new Sword(25,0);
+            Cape cape = new Cape(0,15);
+            warrior.ChangeSword(sword);
+            wizard.ChangeCape(cape);
+            warrior.attackWizard(wizard);
+            int expected = 45;
+            int actual = wizard.Health;
+            Assert.AreEqual(expected, actual);
+        }
     }
-
 }
